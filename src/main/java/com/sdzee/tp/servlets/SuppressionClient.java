ba04fe3e305ea364f9563474f8109313f2e723d2
Bénédicte Mendy <benedicte.mendy@cgi.com>
2019-09-16 20:49:41 +0200

    filter utf8

--- src/main/java/com/sdzee/tp/servlets/SuppressionClient.java
+++ src/main/java/com/sdzee/tp/servlets/SuppressionClient.java
@@ -14,11 +14,12 @@ import com.sdzee.tp.beans.Client;
 @WebServlet("/suppressionClient")
 public class SuppressionClient extends HttpServlet {
 
+	@SuppressWarnings("finally")
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String nomClient = request.getParameter("nom");
 		Map<String, Client> listeClients = (Map<String, Client>) request.getSession().getAttribute("listeClients");
-
+		try {
 		Iterator<String> iterator = listeClients.keySet().iterator();
 		while(iterator.hasNext()) {
 			String clientDeMap = iterator.next();
@@ -27,5 +28,8 @@ public class SuppressionClient extends HttpServlet {
 			}
 		}
 		this.getServletContext().getRequestDispatcher("/listeClients").forward(request, response);
+		} finally {
+			
+		}
 }
 }
